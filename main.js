71eb29c60210bc876156cf14d5dbfd501d07d1d3
alysuarz <60714168+alysuarz@users.noreply.github.com>
2020-12-19 21:55:46 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+
+let spaceAudio = new Audio ();
+spaceAudio.src = "Audio/";
+
+spaceBtn.onclick = function(){
+  spaceAudio.play();
+}
+
+let sAudio = new Audio ();
+sAudio.src = "Audio/";
+
+sBtn.onclick = function(){
+  sAudio.play();
+}
+
+let deAudio = new Audio ();
+dAudio.src = "Audio/";
+
+dBtn.onclick = function(){
+ dAudio.play();
+}
+
+let fAudio = new Audio ();
+fAudio.src = "Audio/";
+
+fBtn.onclick = function(){
+  fAudio.play();
+}
+
+let jAudio = new Audio ();
+jAudio.src = "Audio/";
+
+jBtn.onclick = function(){
+  jAudio.play();
+}
+
+let kAudio = new Audio ();
+kAudio.src = "Audio/";
+
+kBtn.onclick = function(){
+  kAudio.play();
+}
+
+
+let lAudio = new Audio ();
+lAudio.src = "Audio/";
+
+lBtn.onclick = function(){
+  lAudio.play();
+}
+
+
+
+
+
+
+let spaceBtn = document.querySelector(`.space`);
+let sBtn = document.querySelector(`.s`);
+let dBtn = document.querySelector(`.d`);
+let fBtn = document.querySelector(`.f`);
+let jBtn = document.querySelector(`.j`);
+let kBtn = document.querySelector(`.k`);
+let lBtn = document.querySelector(`.l`);
+
+
+let sAudio = document.querySelector(`#saudio`);
+let dAudio = document.querySelector(`#daudio`);
+let fAudio = document.querySelector(`#faudio`);
+let jAudio = document.querySelector(`#jaudio`);
+let kAudio = document.querySelector(`#kaudio`);
+let lAudio = document.querySelector(`#laudio`);
+
